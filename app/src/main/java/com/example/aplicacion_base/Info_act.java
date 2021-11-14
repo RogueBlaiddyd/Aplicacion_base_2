7705328954142b79acd3b6755bc19317e1f4c09f
Rogue <lucianomirandax9@gmail.com>
2021-11-13 21:03:03 -0300

    Funcion maps y llamada funcionando

--- app/src/main/java/com/example/aplicacion_base/Info_act.java
+++ app/src/main/java/com/example/aplicacion_base/Info_act.java
@@ -2,7 +2,10 @@ package com.example.aplicacion_base;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class Info_act extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class Info_act extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+    }
+    
+    public void marcado(View v){
+        Intent i = new Intent(Intent.ACTION_DIAL);
+        i.setData(Uri.parse("tel:"+"+999213123"));
+        startActivity(i);
+    }
+
+    public void maps(View v){
+        Intent i = new Intent(this, MapsActivity.class);
+        startActivity(i);
     }
 }
